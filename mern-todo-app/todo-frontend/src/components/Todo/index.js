e7bcc003c602f00f5778609e04309ea3aa3cf7a1
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-02 18:41:42 +0800

    clone

---
+++ mern-todo-app/todo-frontend/src/components/Todo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+const Todo = () => {
+  return (
+    <Container>
+      <h2>List of ToDos</h2>
+    </Container>
+  )
+}
+
+export default Todo
---
+++ mern-todo-app/todo-frontend/src/components/Todo/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: white;
+  color: gray;
+  border-radius: 15px;
+  /* top, bottom: 20px
+  right, left: 30px */
+  padding: 20px 30px;
+`
+
+export { Container }
