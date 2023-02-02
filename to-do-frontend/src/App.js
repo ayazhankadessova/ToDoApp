fbbaa791bb15e243e133d22c230f7673249a72a2
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-02 18:51:55 +0800

    copy from previous progress

---
+++ to-do-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Welcome to the Todo App!</h2>
+      <Todo />
+    </div>
+  )
+}
+
+export default App
