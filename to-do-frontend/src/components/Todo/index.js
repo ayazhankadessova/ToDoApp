8c4487232c30418a28134f9cd3da409c55a6e691
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-02 18:54:51 +0800

    structure

--- to-do-frontend/src/components/Todo/index.js
+++ to-do-frontend/src/components/Todo/index.js
@@ -5,6 +5,10 @@ const Todo = () => {
   return (
     <Container>
       <h2>List of ToDos</h2>
+      {/* Form Component */}
+      {/* To Do List */}
+      {/* Key - to Do Completed or not? */}
+      {/* giffy */}
     </Container>
   )
 }
