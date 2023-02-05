2d11d72450361f5f505ba961152ee5aca25cf326
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-05 20:44:46 +0800

    axios and check running on port

---
+++ to-do-frontend/src/axios.js
@@ -0,0 +1,18 @@
+// axios converts data in json but it manages headers
+
+import axios from 'axios'
+
+// To use Axios, we have to import it. We have to make it usable in our code.
+// This is how we do that.
+// const axios = require('axios')
+// After doing this, we can use functions from the Axios package.
+// The most notable functions are GET, POST, PATCH, and ALL.
+
+// create object
+const instance = axios.create({
+  // URL which we will fetch data from
+  // port in server js
+  baseURL: 'http://localhost:8000/',
+})
+
+export default instance
