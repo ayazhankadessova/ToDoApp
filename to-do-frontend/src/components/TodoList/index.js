0193cdbd6b5f4b6a71cf2c165662f09284d262ae
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-06 23:35:55 +0800

    update Todo

--- to-do-frontend/src/components/TodoList/index.js
+++ to-do-frontend/src/components/TodoList/index.js
@@ -1,8 +1,24 @@
+import axios from '../../axios'
 import React from 'react'
 import { ListContainer, Row, Text, DeleteSymbol } from './styles'
 
-function TodoList({ todos }) {
+function TodoList({ todos, fetchData }) {
   console.log(todos, 'check')
+
+  // UPDATE ToDO
+  const updateTodo = async (id) => {
+    try {
+      //  The first parameter to axios.put() is the endpoint (passed to requests param), and the 2nd is the HTTP request body.
+      const response = await axios.put(`/todos/${id}`, { id })
+      fetchData()
+      return response.data.json
+      //  updating Todo -> refetch data
+    } catch (err) {
+      // outputs an error message to the Web console.
+      console.error(err.message)
+    }
+  }
+  // Delete ToDo
   return (
     <div>
       {/* todos - bullet points */}
@@ -12,8 +28,14 @@ function TodoList({ todos }) {
         {/* check if todos are there; implicit; render for each todo*/}
         {todos?.map((todo) => (
           <Row key={todo._id}>
-            <Text>{todo.text}</Text>
-            <DeleteSymbol>✖</DeleteSymbol>
+            {/* pass id of element we want to update */}
+            <Text
+              onClick={() => updateTodo(todo._id)}
+              isCompleted={todo.completed === true}
+            >
+              {todo.text}
+            </Text>
+            <DeleteSymbol>X</DeleteSymbol>
           </Row>
         ))}
       </ListContainer>
--- to-do-frontend/src/components/TodoList/styles.js
+++ to-do-frontend/src/components/TodoList/styles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 // ul - unordered list bc order is meaningless
-const ListContainer = styled.ul`
+export const ListContainer = styled.ul`
   padding: 0;
   cursor: pointer;
 `
 // item in the list
-const Row = styled.li`
+export const Row = styled.li`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -15,8 +15,11 @@ const Row = styled.li`
   font-size: 1rem;
 `
 
-const Text = styled.span``
-
-// or styled(Text)
-const DeleteSymbol = styled.span``
-export { ListContainer, Row, Text, DeleteSymbol }
+export const Text = styled.span`
+  ::first-letter {
+    text-transform: capitalize;
+  }
+  color: ${(props) => (props.isCompleted ? 'green' : 'blue')};
+  text-decoration: ${(props) => (props.isCompleted ? 'line-through' : 'none')};
+`
+export const DeleteSymbol = styled(Text)``
